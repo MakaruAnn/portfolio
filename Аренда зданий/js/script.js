e67ce4e1c9128fcb0ddece0f54d569612266e6aa
MakaruAnn <152174042+MakaruAnn@users.noreply.github.com>
2024-11-28 07:40:12 +0300

    Add files via upload

---
+++ Аренда зданий/js/script.js
@@ -0,0 +1,66 @@
+
+new Swiper('.card-wrapper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView: 1
+        },
+        375:{
+          slidesPerView: 1
+      },
+        768:{
+            slidesPerView: 1
+        },
+        1024:{
+            slidesPerView: 3
+        },
+
+    }
+  });
+  new Swiper('.carusel-img', {
+    loop: true,
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
